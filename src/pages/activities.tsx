171941c9fb941d5997be612cd828078da29d9608
banexxl <damjanovic.branislav@gmail.com>
2024-07-03 22:17:57 +0200

    opet isti fix za deploy

--- src/pages/activities.tsx
+++ src/pages/activities.tsx
@@ -51,7 +51,7 @@ const Page = (props: any) => {
      const handleRebuild = async () => {
 
           try {
-               const response = await fetch('https://api.vercel.com/v1/integrations/deploy/prj_XLF2AEk8T1bjTg8qzyJm5wX8wBVT/gLAMvz7E56s', {
+               const response = await fetch('https://api.vercel.com/v1/integrations/deploy/prj_kIxJglN591xV8HcSkVOzbL6T3oLj/grTojSC3fc', {
                     method: 'POST'
                })
 
