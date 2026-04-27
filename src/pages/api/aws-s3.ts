deba6d668f18eb85c3317d5f6d281af75aa10dde
banexxl <damjanovic.branislav@gmail.com>
2026-04-27 17:13:15 +0200

    feat: increase body parser size limit to 10MB in aws-s3 API

--- src/pages/api/aws-s3.ts
+++ src/pages/api/aws-s3.ts
@@ -11,7 +11,7 @@ const s3 = new aws.S3({
 export const config = {
      api: {
           bodyParser: {
-               sizeLimit: '5MB' // Set desired value here
+               sizeLimit: '10MB' // Set desired value here
           }
      }
 }
